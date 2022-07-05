4214d0ba8b7899cabe20c60dff315eddbdead9f6
ZestyBlaze <68081568+ZestyBlaze@users.noreply.github.com>
2022-07-05 15:56:53 +0100

    Multi Type spells now work

--- src/main/java/net/zestyblaze/sorcerycraft/spell/CleanseSpell.java
+++ src/main/java/net/zestyblaze/sorcerycraft/spell/CleanseSpell.java
@@ -3,6 +3,7 @@ package net.zestyblaze.sorcerycraft.spell;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.zestyblaze.sorcerycraft.api.spell.Spell;
 import net.zestyblaze.sorcerycraft.api.spell.SpellType;
@@ -16,6 +17,13 @@ public class CleanseSpell extends Spell {
     public void execute(Level level, Entity source, Entity attacker, Entity target) {
         if(target instanceof LivingEntity) {
             ((LivingEntity)target).removeAllEffects();
+        }
+    }
+
+    @Override
+    public void execute(Level world, ItemStack source, Entity self) {
+        if(self instanceof LivingEntity) {
+            ((LivingEntity)self).removeAllEffects();
         }
     }
 
--- src/main/java/net/zestyblaze/sorcerycraft/spell/HealSpell.java
+++ src/main/java/net/zestyblaze/sorcerycraft/spell/HealSpell.java
@@ -4,6 +4,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.zestyblaze.sorcerycraft.api.spell.Spell;
 import net.zestyblaze.sorcerycraft.api.spell.SpellType;
@@ -17,6 +18,13 @@ public class HealSpell extends Spell {
     public void execute(Level world, Entity source, Entity attacker, Entity target) {
         if (target instanceof LivingEntity) {
             MobEffects.HEAL.applyInstantenousEffect(source, attacker, (LivingEntity)target, getLevel(), 1.0d);
+        }
+    }
+
+    @Override
+    public void execute(Level world, ItemStack source, Entity self) {
+        if (self instanceof LivingEntity) {
+            MobEffects.HEAL.applyInstantenousEffect(self, self, (LivingEntity)self, getLevel(), 1.0d);
         }
     }
 
